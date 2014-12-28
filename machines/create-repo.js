415ae4b9ea258c64479290cc5cb4454c7f1d4e14
Mike McNeil <mike@balderdash.co>
2014-12-28 13:50:08 -0600

    Initial pass at create-repo (not done yet)

---
+++ machines/create-repo.js
@@ -0,0 +1,58 @@
+module.exports = {
+
+  friendlyName: 'Create repo',
+  description: 'Create a new GitHub repository in the specified organization.',
+  cacheable: true,
+
+  inputs: {
+    repo: {
+      description: 'The name for the new Github repo (i.e. as it appears in the URL on GitHub)',
+      example: 'sails',
+      required: true
+    },
+    user: {
+      description: 'The name of the organization or user under which the new repo should be created (i.e. as it appears in the URL on GitHub)',
+      example: 'balderdashy',
+      required: true
+    }
+  },
+
+  defaultExit: 'success',
+
+  exits: {
+    error: {
+      description: 'Unexpected error occurred.'
+    },
+    success: {
+      description: 'Done.'
+    }
+  },
+
+  fn: function(inputs, exits) {
+
+    // Dependencies
+    var _ = require('lodash');
+    var Github = require('github');
+
+
+
+
+    var github = new Github({
+      version: '3.0.0',
+    });
+
+    // TODO: authorization
+
+    github.repos.create(_.pick({
+      has_wiki: false,
+      has_issues: true,
+      description: ''||undefined,
+      homepage: ''||undefined,
+      repo: inputs.repo
+    }, function _stripUndefinedValues(val){ return !_.isUndefined(val); }), function(err, data) {
+      if (err) return exits.error(err);
+      else return exits.success(data);
+    });
+  }
+
+};
