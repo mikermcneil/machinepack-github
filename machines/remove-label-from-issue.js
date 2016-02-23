0640e4f31cb17d36b101bcabb48af27e47d36a5a
Scott Gress <scottmgress@gmail.com>
2016-02-23 16:01:16 +0100

    Add machine for removing label from an issue

---
+++ machines/remove-label-from-issue.js
@@ -0,0 +1,76 @@
+module.exports = {
+
+
+  friendlyName: 'Remove label from issue',
+
+
+  description: 'Remove a label from an issue on GitHub.',
+
+
+  inputs: {
+
+    // Credentials are required to comment on an issue.
+    credentials: require('../structs/credentials.required-input'),
+
+    owner: {
+      description: 'The name of the organization or user that owns the repo (i.e. as it appears in the URL on GitHub)',
+      example: 'balderdashy',
+      required: true
+    },
+
+    repo: {
+      description: 'The name of the Github repo (i.e. as it appears in the URL on GitHub)',
+      example: 'sails',
+      required: true
+    },
+
+    issueNumber: {
+      description: 'The number of the issue to add a label to.',
+      extendedDescription: 'Note that issue numbers are not globally unique-- they are only unique per-repository.',
+      example: 237,
+      required: true
+    },
+
+    label: {
+      description: 'The label to remove.',
+      example: 'Waiting to close',
+      required: true
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'Successfully added labels to issue.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    var Helpers = require('../helpers');
+
+
+    // Close issue
+    Helpers.sendGithubApiRequest({
+
+      method: 'DELETE',
+
+      url: '/repos/'+inputs.owner+'/'+inputs.repo+'/issues/'+inputs.issueNumber+'/labels/' + inputs.label,
+
+      credentials: inputs.credentials
+
+    }).exec({
+      error: exits.error,
+      success: function (apiResponse) {
+        return exits.success();
+      }
+    });
+
+  },
+
+
+};
