2cab44da17493ff2753e3d2f0a94a27fa122f6d2
Scott Gress <scottmgress@gmail.com>
2016-02-23 15:57:41 +0100

    Add machine for adding labels to an issue

---
+++ machines/add-labels-to-issue.js
@@ -0,0 +1,78 @@
+module.exports = {
+
+
+  friendlyName: 'Add labels to issue',
+
+
+  description: 'Add one or more labels to an issue on GitHub.',
+
+
+  inputs: {
+
+    // Credentials are required to comment on an issue.
+    credentials: require('../structs/credentials.required-input'),
+
+    owner: {
+      description: 'The name of the organization or user that owns the repo (i.e. as it appears in the URL on GitHub)',
+      example: 'balderdashy',
+      required: true
+    },
+
+    repo: {
+      description: 'The name of the Github repo (i.e. as it appears in the URL on GitHub)',
+      example: 'sails',
+      required: true
+    },
+
+    issueNumber: {
+      description: 'The number of the issue to add a label to.',
+      extendedDescription: 'Note that issue numbers are not globally unique-- they are only unique per-repository.',
+      example: 237,
+      required: true
+    },
+
+    labels: {
+      description: 'The labels to add.',
+      example: ['Waiting to close'],
+      required: true
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'Successfully added labels to issue.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    var Helpers = require('../helpers');
+
+
+    // Close issue
+    Helpers.sendGithubApiRequest({
+
+      method: 'POST',
+
+      url: '/repos/'+inputs.owner+'/'+inputs.repo+'/issues/'+inputs.issueNumber+'/labels',
+
+      body: inputs.labels,
+
+      credentials: inputs.credentials
+
+    }).exec({
+      error: exits.error,
+      success: function (apiResponse) {
+        return exits.success();
+      }
+    });
+
+  },
+
+
+};
