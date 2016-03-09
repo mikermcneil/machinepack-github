2a56cd1bbceea63b62ce6d65f77551381ef35108
Scott Gress <scottmgress@gmail.com>
2016-03-09 14:00:06 -0600

    Added machines for listing and editing webhooks

---
+++ machines/edit-webhook.js
@@ -0,0 +1,110 @@
+module.exports = {
+
+
+  friendlyName: 'Edit webhook',
+
+
+  description: 'Edit a webhook in a repo',
+
+
+  inputs: {
+
+    // Credentials are required to comment on an issue.
+    credentials: require('../structs/credentials.required-input'),
+
+    owner: {
+      description: 'The name of the organization or user that owns the repo (i.e. as it appears in the URL on GitHub)',
+      example: 'balderdashy',
+      required: true
+    },
+
+    repo: {
+      description: 'The name of the Github repo (i.e. as it appears in the URL on GitHub)',
+      example: 'sails',
+      required: true
+    },
+
+    id: {
+      description: 'The ID of the hook to edit',
+      example: 123,
+      required: true
+    },
+
+    config: {
+      description: 'The configuration of the webhook.',
+      example: {},
+      required: true,
+      moreInfoUrl: 'https://developer.github.com/v3/repos/hooks/#create-a-hook'
+    },
+
+    events: {
+      description: 'List of events to replace current hook events list with',
+      example: ['push'],
+    },
+
+    addEvents: {
+      description: 'List of events to add to current hook events list',
+      example: ['push'],
+    },
+
+    removeEvents: {
+      description: 'List of events to remove from current hook events list',
+      example: ['push'],
+    },
+
+    active: {
+      description: 'Whether or not the hook should be activated',
+      example: true,
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'Successfully added webhook to repo.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+     // Dependencies
+    var _ = require('lodash');
+    var Github = require('github');
+    var Machine = require('machine');
+    var github = new Github({
+      version: '3.0.0',
+    });
+
+    var Helpers = require('../helpers');
+
+    // Close issue
+    Helpers.sendGithubApiRequest({
+
+      method: 'PATCH',
+
+      url: '/repos/'+inputs.owner+'/'+inputs.repo+'/hooks/' + inputs.id,
+
+      params: {
+        config: inputs.config,
+        events: inputs.events,
+        add_events: inputs.addEvents,
+        remove_events: inputs.removeEvents,
+        active: inputs.active
+      },
+
+      credentials: inputs.credentials
+
+    }).exec({
+      error: exits.error,
+      success: function (apiResponse) {
+        return exits.success();
+      }
+    });
+
+  }
+
+};
---
+++ machines/list-webhooks.js
@@ -0,0 +1,88 @@
+module.exports = {
+
+
+  friendlyName: 'List webhooks',
+
+
+  description: 'List all webhooks in a repo',
+
+
+  inputs: {
+
+    // Credentials are required to comment on an issue.
+    credentials: require('../structs/credentials.required-input'),
+
+    owner: {
+      description: 'The name of the organization or user that owns the repo (i.e. as it appears in the URL on GitHub)',
+      example: 'balderdashy',
+      required: true
+    },
+
+    repo: {
+      description: 'The name of the Github repo (i.e. as it appears in the URL on GitHub)',
+      example: 'sails',
+      required: true
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      example: [
+        {
+          "id": 1,
+          "url": "https://api.github.com/repos/octocat/Hello-World/hooks/1",
+          "test_url": "https://api.github.com/repos/octocat/Hello-World/hooks/1/test",
+          "ping_url": "https://api.github.com/repos/octocat/Hello-World/hooks/1/pings",
+          "name": "web",
+          "events": [
+            "push",
+            "pull_request"
+          ],
+          "active": true,
+          "config": {
+            "url": "http://example.com/webhook",
+            "content_type": "json"
+          },
+          "updated_at": "2011-09-06T20:39:23Z",
+          "created_at": "2011-09-06T17:26:27Z"
+        }
+      ]
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+     // Dependencies
+    var _ = require('lodash');
+    var Github = require('github');
+    var Machine = require('machine');
+    var github = new Github({
+      version: '3.0.0',
+    });
+
+    var Helpers = require('../helpers');
+
+    // Close issue
+    Helpers.sendGithubApiRequest({
+
+      method: 'GET',
+
+      url: '/repos/'+inputs.owner+'/'+inputs.repo+'/hooks',
+
+      credentials: inputs.credentials
+
+    }).exec({
+      error: exits.error,
+      success: function (apiResponse) {
+        return exits.success(apiResponse.body);
+      }
+    });
+
+  }
+
+};
