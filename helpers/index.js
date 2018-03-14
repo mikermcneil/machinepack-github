007921314c42c2b083f8b6b5d7eb4c7479725f24
Mike McNeil <git+mikermcneil@sailsjs.com>
2018-03-14 13:29:47 -0500

    Add eslint, rename things, and do some other normalization

--- helpers/index.js
+++ helpers/index.js
@@ -3,6 +3,6 @@ var _ = require('lodash');
 var Machine = require('machine');
 
 module.exports = {
-  normalizeCredentials: Machine.build(_.extend({identity: 'normalize-credentials' }, require('./machines/normalize-credentials'))),
-  sendGithubApiRequest: Machine.build(_.extend({identity: 'send-github-api-request' }, require('./machines/send-github-api-request'))),
+  normalizeCredentials: Machine.build(_.extend({identity: 'normalize-credentials' }, require('./lib/normalize-credentials'))),
+  sendGithubApiRequest: Machine.build(_.extend({identity: 'send-github-api-request' }, require('./lib/send-github-api-request'))),
 };
