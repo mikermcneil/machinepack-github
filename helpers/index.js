039be9a3d76c5afd3b5c8313a6b163a4be9ba8c4
Rachael Shaw <r@rachael.wtf>
2019-04-17 11:46:44 -0500

    Intermediate commit - moved helpers into `lib/private/`, but need to update all the places using the helpers

--- helpers/index.js
+++
@@ -1,8 +0,0 @@
-// This is a little helper pack with some private machines.
-var _ = require('@sailshq/lodash');
-var Machine = require('machine');
-
-module.exports = {
-  normalizeCredentials: Machine.build(_.extend({identity: 'normalize-credentials' }, require('./lib/normalize-credentials'))),
-  sendGithubApiRequest: Machine.build(_.extend({identity: 'send-github-api-request' }, require('./lib/send-github-api-request'))),
-};
