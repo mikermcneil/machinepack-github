98e244e23956b9dc6eeb9838a57a99e1e1f02c40
Rachael Shaw <r@rachael.wtf>
2019-04-17 11:43:40 -0500

    Rename `structs/` to `constants/`

--- helpers/machines/send-github-api-request.js
+++ helpers/machines/send-github-api-request.js
@@ -44,7 +44,7 @@ module.exports = {
     },
 
     // Credentials are optional
-    credentials: require('../../structs/credentials.optional-input'),
+    credentials: require('../../constants/credentials.optional-input'),
 
   },
 
